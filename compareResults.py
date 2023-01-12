9c946cfb80747374c01ca4e0829385679a6975a2
0x00Duke <leon.ducasse@epitech.eu>
2023-01-12 21:03:39 +0100

    [ADD/MODIFY] plot distribution percentage of guesses and percentage of right guess

--- compareResults.py
+++ compareResults.py
@@ -2,11 +2,14 @@
 
 import os
 from sys import argv as av
+from dataclasses import astuple, dataclass, field, fields
 import pandas as pd
 import time
 import requests
 import json
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 load_dotenv()
 
@@ -18,6 +21,45 @@ consensus = {
     "L": "Lighthouse",
     "T": "Teku"
 }
+
+clients = [
+    "Lighthouse",
+    "Lodestar",
+    "Nimbus",
+    "Prysm",
+    "Teku"
+]
+
+
+@dataclass
+class BlockGuess:
+    slot: int
+    proposer_index: int
+    best_guess_single: str
+    best_guess_multi: str
+    probability_map: dict
+
+    @classmethod
+    def from_json(cls, json):
+        return cls(**json)
+
+@dataclass
+class ProbabilityMapArray:
+    Lighthouse: list = field(default_factory=list)
+    Lodestar: list = field(default_factory=list)
+    Nimbus: list = field(default_factory=list)
+    Prysm: list = field(default_factory=list)
+    Teku: list = field(default_factory=list)
+
+    def add_elem(self, Lighthouse, Lodestar, Nimbus, Prysm, Teku):
+        self.Lighthouse.append(Lighthouse)
+        self.Lodestar.append(Lodestar)
+        self.Nimbus.append(Nimbus)
+        self.Prysm.append(Prysm)
+        self.Teku.append(Teku)
+    
+    def getLists(self):
+        return [self.Lighthouse, self.Lodestar, self.Nimbus, self.Prysm, self.Teku]
 
 
 def parseGraffiti(f_graffiti):
@@ -76,14 +118,14 @@ def getBlockprintGuesses(init_block, end_block):
     return: a dictionary with the block slot as key and the best guess as value
     '''
 
-    bestGuesses = {}
+    guesses = {}
     # call to the API to get the guesses
     block_guesses = get_validator_proposed_blocks(init_block, end_block)
 
-    # get the best guess for each block and add it to the dictionary with the slot as key
+    # get the guess for each block and add it to the dictionary with the slot as key
     for block in block_guesses:
-        bestGuesses[block['slot']] = block['best_guess_single']
-    return bestGuesses
+        guesses[block['slot']] = BlockGuess.from_json(block)
+    return guesses
 
 
 def compareGuesses(rocketDF, bestGuesses):
@@ -92,14 +134,93 @@ def compareGuesses(rocketDF, bestGuesses):
     return: a list of booleans, True if the guess is matching, False otherwise
     '''
 
-    # initialize the list of booleans to gain time
+    # initialize the lists to gain time
     matches = [True] * len(rocketDF)
+    wrongGuesses = [None] * len(rocketDF)
+    proba_map_array = ProbabilityMapArray()
 
     # compare the guesses for the blocks we have known clients for
     # and add the result to the list in order to add it to the dataframe
     for i in range(len(rocketDF)):
-        matches[i] = rocketDF['f_client'].iloc[i] == bestGuesses[rocketDF['f_slot'].iloc[i]]
-    return matches
+        block_guess = bestGuesses[rocketDF['f_slot'].iloc[i]]
+        matches[i] = rocketDF['f_client'].iloc[i] in block_guess.best_guess_single
+
+        proba_map = block_guess.probability_map
+        proba_map_array.add_elem(*proba_map.values())
+
+        if not matches[i]:
+            wrongGuesses[i] = block_guess.best_guess_single
+
+    return matches, wrongGuesses, proba_map_array
+
+
+def analyzeResults(rocketDF):
+    def drawValues():
+        y_offset = -5
+        for bar in plt.gca().patches:
+            ax = plt.gca()
+            if (bar.get_height() < 5):
+                continue
+            ax.text(
+                bar.get_x() + bar.get_width() / 2,
+                bar.get_height() + bar.get_y() + y_offset,
+                '{:.2f}%'.format(bar.get_height()),
+                ha='center',
+                color='black',
+                weight='bold',
+                fontsize=8
+            )
+    
+    def setTitleAndLabels(title, xlabel, ylabel):
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+    
+    def plotBarChart(df, title, xlabel, ylabel):
+        columns = ['f_client', 'proba_Lighthouse', 'proba_Nimbus', 'proba_Prysm', 'proba_Teku']
+
+        # get the distribution of the clients guesses thanks to the mean values
+        distribution = df[columns].groupby('f_client').mean()
+        
+        # change from decimal to percentage
+        percentDistribution = distribution.div(distribution.sum(axis=1), axis=0) * 100
+
+        # plot the bar chart
+        percentDistribution.plot(kind='bar', stacked=True, title=title)
+        
+        # change the titles and labels
+        setTitleAndLabels(title, xlabel, ylabel)
+
+        # draw the values on the bars
+        drawValues()
+
+    def getVisualRepresentation():
+        # plot the distribution of the clients for all the blocks
+        plotBarChart(rocketDF, 'Distribution of the clients guesses for all the blocks', 'Clients', 'Percentage (%)')
+        
+        # plot the distribution of the clients for the unmatched blocks
+        plotBarChart(unmatchedDF, 'Distribution of the clients guesses for the unmatched blocks', 'Clients', 'Percentage (%)')
+        plt.show()
+    
+    def getMatchingPercentageForEach():
+        col = ['f_client','match']
+        a = rocketDF[col]
+        a = a.groupby('f_client').mean().round(4) * 100
+        print(a)
+
+    # get the unmatched blocks and get the distribution of the clients
+    unmatchedDF = rocketDF[rocketDF['match'] == False]
+
+
+    # uncomment the line below to display the guesses distribution charts
+    # getVisualRepresentation()
+
+    # uncomment the line below to display the matching percentage for each client
+    # getMatchingPercentageForEach()
+
+    # get the matching percentage and print it
+    matchingPercentage = rocketDF.match.value_counts()[True] / len(rocketDF) * 100
+    print('We have a matching percentage of {:.2f}%'.format(matchingPercentage))
 
 
 def compareResults(fileName):
@@ -122,15 +243,19 @@ def compareResults(fileName):
     bestGuesses = getBlockprintGuesses(init_block, end_block + 1)
 
     # compare the guesses and add the result to the dataframe
-    rocketDF['match'] = compareGuesses(rocketDF, bestGuesses)
+    matching, wrongGuesses, probaMap = compareGuesses(rocketDF, bestGuesses)
+    rocketDF['match'] = matching
+    rocketDF['model_guess'] = wrongGuesses
+    
+    allProbabilities = probaMap.getLists()
+    for i, client in enumerate(clients):
+        rocketDF[f'proba_{client}'] = allProbabilities[i]
 
     # uncomment the line below to save the results to a csv file
-    # rocketDF.to_csv('result.csv')
+    rocketDF.to_csv('result.csv')
 
-    # get the matching percentage and print it
-    matchingPercentage = rocketDF.match.value_counts()[True] / len(rocketDF) * 100
-    print('We have a matching percentage of {:.2f}%'.format(
-        matchingPercentage))
+    # analyze the results
+    analyzeResults(rocketDF)
 
     # end timer and print the time it took to compare the results
     end = time.time()
@@ -138,8 +263,11 @@ def compareResults(fileName):
 
 
 def main():
+
     fileName = 'rocket-pool-proposals.csv' if len(av) < 2 else av[1]
     compareResults(fileName)
+    # df = pd.read_csv('result.csv')
+    # analyzeResults(df)
 
 
 if __name__ == "__main__":
