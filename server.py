394dbeda8ccd772e4f026e2881ecb4040b076dec
0x00Duke <leon.ducasse@epitech.eu>
2023-02-20 16:48:37 +0100

    [CHG] pass node url and model folder as arg when launching server

--- server.py
+++ server.py
@@ -2,17 +2,19 @@
 
 from flask import Flask, request, jsonify
 from guessRequester import getSlotGuess
+import argparse
 
 app = Flask(__name__)
+
+node_url = 'http://localhost:5052'
+model_folder = 'blockprint/model/'
+add_to_model = None
 
 @app.route('/getClientGuess', methods=['GET'])
 def getClientGuess():
     args = (request.args).to_dict()
 
     slot = args.get('slot')
-    model_folder = args.get('model_folder')
-    add_to_model= args.get('add_to_model') is not None
-    node_url = args.get('node_url') or 'http://localhost:5052'
 
     if slot is None:
         return jsonify({'error': 'Invalid request, please provide slot'}), 500
@@ -28,5 +30,17 @@ def getClientGuess():
 
     return jsonify(res), 200
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Request a guess for a given slot')
+    parser.add_argument('model_folder', default=model_folder, type=str, help='Path to the folder with model files')
+    parser.add_argument('--add-to-model', default=add_to_model, action='store_true',help='Add the block to the model if client could be identified with graffiti')
+    parser.add_argument('--node-url', default=node_url, type=str, help='URL of the beacon node to download blocks from (default: http://localhost:5052)')
+    return parser.parse_args()
+
 if __name__ == "__main__":
+    args = parse_args()
+    model_folder = args.model_folder
+    add_to_model = args.add_to_model is not None
+    node_url = args.node_url
+    print(add_to_model)
     app.run()
